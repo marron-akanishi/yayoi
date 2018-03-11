c63d9965f60c350fafa02db139832f190b2151e7
Marron Akanishi <marron.general@gmail.com>
2018-03-11 16:20:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# imas_detect
+ソースはほぼ[ディープラーニングでザッカーバーグの顔を識別するAIを作る - Qiita](https://qiita.com/AkiyoshiOkano/items/72f3e4ba9caf514460ee)からお借りしたもの  
---
+++ web.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug import secure_filename
+import pic_eval
+
+# 自身の名称を app という名前でインスタンス化する
+app = Flask(__name__)
+app.config['DEBUG'] = True
+# 投稿画像の保存先
+UPLOAD_FOLDER = './static/images/default'
+
+# ルーティング。/にアクセス時
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+# 画像投稿時のアクション
+@app.route('/post', methods=['GET','POST'])
+def post():
+  if request.method == 'POST':
+    if not request.files['file'].filename == '':
+      # アップロードされたファイルを保存
+      f = request.files['file']
+      img_path = os.path.join(UPLOAD_FOLDER, secure_filename(f.filename))
+      f.save(img_path)
+      # pic_eval.pyへアップロードされた画像を渡す
+      result = pic_eval.evaluation(img_path, './imas_model.ckpt')
+    else:
+      result = []
+    return render_template('index.html', result=result)
+  else:
+  	# エラーなどでリダイレクトしたい場合
+  	return redirect(url_for('index'))
+
+if __name__ == '__main__':
+  app.debug = True
+  app.run(host='0.0.0.0')
