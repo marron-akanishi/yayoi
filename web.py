45b8c653f5612498d605c82c88bee1c282401b17
Marron Akanishi <marron.general@gmail.com>
2018-04-05 20:04:42 +0900

    画像保存処理変更

--- web.py
+++ web.py
@@ -1,7 +1,8 @@
 import os
 import json
 import uuid
-from flask import Flask, render_template, request
+from datetime import datetime
+from flask import Flask, render_template, request, make_response, redirect
 import pic_eval
 
 OTHER = 13
@@ -15,24 +16,28 @@ CHARA_NAMES = json.load(open("./chara.json", encoding="utf-8"))
 
 # ルーティング。/にアクセス時
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index(error=False):
+    res = make_response(render_template('index.html', error=error))
+    if request.cookies.get('uid', None) == None:
+        res.set_cookie('uid', str(uuid.uuid4()))
+    return res
 
 # 画像投稿時のアクション
 @app.route('/detect', methods=['POST'])
 def post():
     result = None
-    if not request.files['file'].filename == '':
+    filename = request.cookies.get('uid', None)
+    if not request.files['file'].filename == '' and filename != None:
         # アップロードされたファイルを保存
         f = request.files['file']
         if f.filename.split('.')[-1] in ['png', 'jpg']:
-            filename = str(uuid.uuid4()) + "." + f.filename.split('.')[-1]
+            filename += "." + f.filename.split('.')[-1]
             img_path = UPLOAD_FOLDER + filename
             f.save(img_path)
             # pic_eval.pyへアップロードされた画像を渡す
             result = pic_eval.evaluation(img_path, './imas_model.ckpt')
     if result == None:
-        return render_template('index.html', error=True)
+        return index(True)
     else:
         isAS = False
         rect = [[],[],[],[]]
@@ -44,8 +49,10 @@ def post():
             #if chara['rank'][0]['label'] != OTHER:
             if chara['rank'][0]['rate'] >= 90:
                 isAS = True
+        timestamp = int(datetime.now().timestamp())
+        img_path += "?time={}".format(timestamp)
         return render_template('index.html', img_path=img_path[1:], rect=rect, result=result, isAS=isAS)
 
 if __name__ == '__main__':
-    app.debug = True
+    app.debug = False
     app.run(host='0.0.0.0')
